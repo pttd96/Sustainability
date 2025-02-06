5231a024ed5046003f694e00dad6207f88788299
Admin <admin@192.168.1.7>
2025-02-06 23:52:36 +0700

    Initial commit

---
+++ src/main/java/com/miniproject/sustainability/SustainabilityApplication.java
@@ -0,0 +1,13 @@
+package com.miniproject.sustainability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SustainabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SustainabilityApplication.class, args);
+	}
+
+}
